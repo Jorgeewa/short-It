9c0102acf519d6959740f8c5831884e4fcea55a2
Jorgeewa <georgeewauche@yahoo.com>
2017-02-04 17:17:39 +0100

    changed the frontend
    redisgned the front end

--- app/main/main-controller.js
+++ app/main/main-controller.js
@@ -16,5 +16,56 @@
             }).catch(function(error){
                 console.log(error);
             })
+            
+            
+            $scope.openTab = function(activate){
+                var mostActive = angular.element(document.querySelector(".most-active"))[0];
+                var mostAdvanced = angular.element(document.querySelector(".most-advanced"))[0];
+                var mostDeclined = angular.element(document.querySelector(".most-declined"))[0];
+                var unusualVolume = angular.element(document.querySelector(".unusual-volume"))[0];
+                    switch(activate){
+                        case 1:
+                            $scope.activeTabOne = true;
+                            $scope.activeTabTwo = false;
+                            $scope.activeTabThree = false;
+                            $scope.activeTabFour = false;
+                            console.log(mostActive[0])
+                            mostActive.style.display = "block";
+                            mostAdvanced.style.display = "none";
+                            mostDeclined.style.display = "none";
+                            unusualVolume.style.display = "none";
+                        break;
+                        case 2:
+                            $scope.activeTabTwo = true;
+                            $scope.activeTabOne = false;
+                            $scope.activeTabThree = false;
+                            $scope.activeTabFour = false;
+                            mostActive.style.display = "none";
+                            mostAdvanced.style.display = "block";
+                            mostDeclined.style.display = "none";
+                            unusualVolume.style.display = "none";
+                        break;
+                        case 3:
+                            $scope.activeTabTwo = false;
+                            $scope.activeTabOne = false;
+                            $scope.activeTabThree = true;
+                            $scope.activeTabFour = false;
+                            mostActive.style.display = "none";
+                            mostAdvanced.style.display = "none";
+                            mostDeclined.style.display = "block";
+                            unusualVolume.style.display = "none";
+                        break;
+                        case 4:
+                            $scope.activeTabTwo = false;
+                            $scope.activeTabOne = false;
+                            $scope.activeTabThree = false;
+                            $scope.activeTabFour = true;
+                            mostActive.style.display = "none";
+                            mostAdvanced.style.display = "none";
+                            mostDeclined.style.display = "none";
+                            unusualVolume.style.display = "block";
+                        break;
+                    }
+                }
         }])
 }())
